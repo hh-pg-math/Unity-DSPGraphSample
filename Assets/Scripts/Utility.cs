5d1c71745a464321562bd89ecebca85b0f2d2b16
MAX-eipi <max.eipi.3141@gmail.com>
2020-03-01 18:58:42 +0900

    create Utility

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Mathematics;
+
+public static class Utility
+{
+    // 69 = 440 Hz
+    public static float NoteNumberToFrequency(int noteNumber)
+    {
+        return math.pow(2, (noteNumber - 69) / 12.0f) * 440.0f;
+    }
+}
